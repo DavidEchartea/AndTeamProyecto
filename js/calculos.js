88f91357db35e5af586d56ab6b37d578cc4302da
David Echartea <44214497+DavidEchartea@users.noreply.github.com>
2019-11-01 13:50:22 -0600

    arreglando tus cagazones

--- js/calculos.js
+++ js/calculos.js
@@ -6,7 +6,7 @@ function catchyMc(){
 	var suma = parseInt(pecho) + parseInt(abs) + parseInt(cuadricep);
 	var densidad  = ((0.0008267 * parseInt(suma)) + (0.0000016 * parseInt(edad)) - (0.0002574 * parseInt(edad)));
 	var porcentajeGrasa = (495 / parseFloat(densidad) - 450);
-	document.getElementById('txtCatch').vale = porcentajeGrasa.toFixed(4);
+	document.getElementById('txtCatch').value = porcentajeGrasa.toFixed(4);
 }
 function calcularIMC(){
 	var peso = document.getElementById('txtPesoAct').value;
